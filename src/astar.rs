078b8090d97efffa6485acbe9bda8da834b25504
Martin Hamel <martin@komunide.com>
2023-03-17 16:29:52 +0000

    this version is working but is very slow

--- src/astar.rs
+++ src/astar.rs
@@ -2,237 +2,188 @@
 //! algorithm](https://en.wikipedia.org/wiki/A*_search_algorithm).
 
 use actix_web::web::Data;
-use indexmap::map::Entry::{Occupied, Vacant};
-use indexmap::IndexMap;
-use num_traits::Zero;
-use rustc_hash::FxHasher;
 use std::cmp::Ordering;
-use std::collections::BinaryHeap;
-use std::hash::{BuildHasherDefault, Hash};
+use std::collections::HashMap;
 use std::sync::mpsc::channel;
-use std::sync::{Arc, Mutex};
-use std::usize;
-
+use std::sync::{Arc, Mutex, RwLock};
 use crate::data::node::Node;
 use crate::AppState;
 
-type FxIndexMap<K, V> = IndexMap<K, V, BuildHasherDefault<FxHasher>>;
-
-/// Compute a shortest path using the [A* search
-/// algorithm](https://en.wikipedia.org/wiki/A*_search_algorithm).
-///
-/// The shortest path starting from `start` up to a node for which `success` returns `true` is
-/// computed and returned along with its total cost, in a `Some`. If no path can be found, `None`
-/// is returned instead.
-///
-/// - `start` is the starting node.
-/// - `successors` returns a list of successors for a given node, along with the cost for moving
-/// from the node to the successor. This cost must be non-negative.
-/// - `heuristic` returns an approximation of the cost from a given node to the goal. The
-/// approximation must not be greater than the real cost, or a wrong shortest path may be returned.
-/// - `success` checks whether the goal has been reached. It is not a node as some problems require
-/// a dynamic solution instead of a fixed node.
-///
-/// A node will never be included twice in the path as determined by the `Eq` relationship.
-///
-/// The returned path comprises both the start and end node.
-///
-/// # Example
-///
-/// We will search the shortest path on a chess board to go from (1, 1) to (4, 6) doing only knight
-/// moves.
-///
-/// The first version uses an explicit type `Pos` on which the required traits are derived.
-///
-/// ```
-/// use pathfinding::prelude::astar;
-///
-/// #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-/// struct Pos(i32, i32);
-///
-/// impl Pos {
-///   fn distance(&self, other: &Pos) -> u32 {
-///     (self.0.abs_diff(other.0) + self.1.abs_diff(other.1)) as u32
-///   }
-///
-///   fn successors(&self) -> Vec<(Pos, u32)> {
-///     let &Pos(x, y) = self;
-///     vec![Pos(x+1,y+2), Pos(x+1,y-2), Pos(x-1,y+2), Pos(x-1,y-2),
-///          Pos(x+2,y+1), Pos(x+2,y-1), Pos(x-2,y+1), Pos(x-2,y-1)]
-///          .into_iter().map(|p| (p, 1)).collect()
-///   }
-/// }
-///
-/// static GOAL: Pos = Pos(4, 6);
-/// let result = astar(&Pos(1, 1), |p| p.successors(), |p| p.distance(&GOAL) / 3,
-///                    |p| *p == GOAL);
-/// assert_eq!(result.expect("no path found").1, 4);
-/// ```
-///
-/// The second version does not declare a `Pos` type, makes use of more closures,
-/// and is thus shorter.
-///
-/// ```
-/// use pathfinding::prelude::astar;
-///
-/// static GOAL: (i32, i32) = (4, 6);
-/// let result = astar(&(1, 1),
-///                    |&(x, y)| vec![(x+1,y+2), (x+1,y-2), (x-1,y+2), (x-1,y-2),
-///                                   (x+2,y+1), (x+2,y-1), (x-2,y+1), (x-2,y-1)]
-///                               .into_iter().map(|p| (p, 1)),
-///                    |&(x, y)| (GOAL.0.abs_diff(x) + GOAL.1.abs_diff(y)) / 3,
-///                    |&p| p == GOAL);
-/// assert_eq!(result.expect("no path found").1, 4);
-/// ```
-#[allow(clippy::missing_panics_doc)]
 pub fn astar(start: Node, end: Node, state: Data<AppState>) -> Option<(Vec<Node>, i64)> {
     let pool = rayon::ThreadPoolBuilder::new()
         .num_threads(22)
         .build()
         .unwrap();
-    let response: Arc<Mutex<Option<(Vec<Node>, i64)>>> = Arc::new(Mutex::new(None));
+    let response: Arc<RwLock<Option<Arc<RwLock<SearchSate>>>>> = Arc::new(RwLock::new(None));
+    let start = start.clone();
     let response_clone = response.clone();
+    let state_clone = state.clone();
+    let node_id_search_state_map = Arc::new(Mutex::new(HashMap::new()));
+    let node_id_search_state_map_clone = node_id_search_state_map.clone();
     pool.scope(move |scope| {
+        let node_id_search_state_map = node_id_search_state_map_clone;
+        let state = state_clone;
         let response = response_clone;
         let (tx_to_see_push, rx_to_see_push) = channel();
-        let to_see = Arc::new(Mutex::new(BinaryHeap::new()));
-        to_see.lock().unwrap().push(SmallestCostHolder {
-            estimated_cost: Zero::zero(),
-            cost: Zero::zero(),
-            index: 0,
-        });
-        let parents = Arc::new(Mutex::new(FxIndexMap::default()));
-        parents
+        let to_see = Arc::new(RwLock::new(Vec::new()));
+        let search_state = Arc::new(RwLock::new(SearchSate {
+            cost: 0,
+            node: start.clone(),
+            parent_id: None,
+        }));
+        node_id_search_state_map
             .lock()
             .unwrap()
-            .insert(start.clone(), (usize::max_value(), Zero::zero()));
+            .insert(start.id, search_state.clone());
+        to_see.write().unwrap().push(search_state.clone());
         loop {
-            if response.lock().unwrap().is_some() {
-                break;
+            // We already have the response
+            if response.read().unwrap().is_some() {
+                return;
             }
-            let mut guard = to_see.lock().unwrap();
-            let (cost, index) = match guard.pop() {
-                Some(SmallestCostHolder { cost, index, .. }) => (cost, index),
+            let mut a_voir = "".to_string();
+            to_see.read().unwrap().iter().for_each(|state| {
+                a_voir = a_voir.to_owned() + &state.read().unwrap().node.distance(&end).to_string() + "-" + &state.read().unwrap().cost.to_string() +  ", ";
+            });
+            println!("a voir: {}", a_voir);
+            let mut locked_to_see = to_see.write().unwrap();
+            let search_state = match locked_to_see.pop() {
+                Some(search_state) => search_state,
                 None => {
-                    drop(guard);
+                    drop(locked_to_see);
                     rx_to_see_push.recv().unwrap();
                     continue;
                 }
             };
-            drop(guard);
-            let to_see = to_see.clone();
-            let parents = parents.clone();
+            drop(locked_to_see);
+            println!("distance: {}", search_state.read().unwrap().node.distance(&end));
             let tx_to_see_push = tx_to_see_push.clone();
             let end = end.clone();
-            let state = state.clone();
+            let search_state = search_state.clone();
             let response = response.clone();
+            let to_see = to_see.clone();
+            let state = state.clone();
+            let node_id_search_state_map = node_id_search_state_map.clone();
             scope.spawn(move |_scope| {
-                if response.lock().unwrap().is_some() {
+                if response.read().unwrap().is_some() {
                     return;
                 }
-                let successors = {
-                    let guard = parents.lock().unwrap();
-                    let guard2 = guard.clone();
-                    let (node, &(_, c)) = guard2.get_index(index).unwrap(); // Cannot fail
-                    if node.lat == end.lat && node.lon == end.lon {
-                        println!("Found path! Cost: {}", cost);
-                        let path = reverse_path(guard2.clone(), |&(p, _)| p, index);
-                        response.lock().unwrap().replace((path, cost)) ;
-                        return;
-                    }
-                    drop(guard);
-                    // We may have inserted a node several time into the binary heap if we found
-                    // a better way to access it. Ensure that we are currently dealing with the
-                    // best path and discard the others.
-                    if cost > c {
-                        // tx.send(None).unwrap();
-                        return;
-                    }
-                    node.successors(state).unwrap()
-                };
+                let state_locked = search_state.read().unwrap();
+                if state_locked.node.lat == end.lat && state_locked.node.lon == end.lon {
+                    response.write().unwrap().replace(search_state.clone());
+                    return;
+                }
+                let successors = state_locked.node.successors(state).unwrap();
                 for (successor, move_cost) in successors {
-                    let new_cost = cost as i64 + move_cost;
-                    let h; // heuristic(&successor)
-                    let n; // index for successor
-                    match parents.lock().unwrap().entry(successor.clone()) {
-                        Vacant(e) => {
-                            h = successor.distance(&end);
-                            n = e.index();
-                            e.insert((index, new_cost));
-                        }
-                        Occupied(mut e) => {
-                            if e.get().1 > new_cost {
-                                h = successor.distance(e.key());
-                                n = e.index();
-                                e.insert((index, new_cost));
-                            } else {
+                    let new_cost = state_locked.cost + move_cost;
+                    let h = successor.distance(&end); // heuristic(&successor)
+                    let mut to_see = to_see.write().unwrap();
+                    let mut node_id_search_state_map = node_id_search_state_map.lock().unwrap();
+                    match node_id_search_state_map.get(&successor.id) {
+                        Some(search_state) => {
+                            if search_state.read().unwrap().cost <= new_cost + h as i64 {
+                                // We already have a better path to this node
                                 continue;
                             }
+                            let new_state = Arc::new(RwLock::new(SearchSate {
+                                cost: new_cost + h as i64,
+                                node: successor.clone(),
+                                parent_id: Some(state_locked.node.id),
+                            }));
+                            // Replace the old state with the new one
+                            node_id_search_state_map.insert(successor.id, new_state.clone());
+                            // Remove the old state from the to_see list
+                            to_see.retain(|state| state.read().unwrap().node.id != successor.id);
+                            // Add the new state to the to_see list
+                            to_see.push(new_state);
+                        }
+                        None => {
+                            let new_state = Arc::new(RwLock::new(SearchSate {
+                                cost: new_cost + h as i64,
+                                node: successor.clone(),
+                                parent_id: Some(state_locked.node.id),
+                            }));
+                            node_id_search_state_map.insert(successor.id, new_state.clone());
+                            to_see.push(new_state);
                         }
                     }
-                    let mut to_see = to_see.lock().unwrap();
-                    to_see.push(SmallestCostHolder {
-                        estimated_cost: new_cost + h as i64,
-                        cost: new_cost,
-                        index: n,
-                    });
+                    to_see.sort_by(|b, a| a.read().unwrap().cost.cmp(&b.read().unwrap().cost));
                     tx_to_see_push.send(true).unwrap_or_else(|e| {
-                        println!("Failed to send to to_see_push {:?}", e);
+                        println!("Failed to end to to_see_push {:?}", e);
+                        return;
                     });
                 }
             });
         }
     });
-    println!("Got result");
-    //returns the response
-    let x = response.lock().unwrap().clone(); 
-    x
+
+    // Prepare response
+    let mut transformed_response = Vec::new();
+    println!("transforming response...");
+    loop {
+        println!("looping...");
+        let mut locked_response = response.write().unwrap();
+        match locked_response.clone() {
+            Some(search_state) => {
+                let locked_search_state = search_state.read().unwrap();
+                transformed_response.push(locked_search_state.node.clone());
+                println!("pushed");
+                match locked_search_state.parent_id {
+                    Some(pid) => {
+                        println!("parent_id: {:?}", locked_search_state.parent_id);
+                        let locked_node_id_search_state_map = node_id_search_state_map.lock().unwrap();
+                        let next_search_state = locked_node_id_search_state_map
+                            .get(&pid)
+                            .unwrap()
+                            .clone();
+                        println!("next_search_state: {:?}", next_search_state.read().unwrap().node.id);
+                        locked_response.replace(next_search_state.clone());
+                        println!("transformed response: {:?}", transformed_response);
+                    }
+                    None => {
+                        println!("search state.");
+                        break;
+                    }
+                }
+            }
+            None => {
+                println!("No parent_id. Breaking.");
+                break;
+            }
+        }
+    }
+    transformed_response.reverse();
+    println!("transformed response: {:?}", transformed_response);
+    let r = response.read().unwrap().clone();
+    Some((transformed_response, r.unwrap().read().unwrap().cost))
 }
 
-struct SmallestCostHolder<K> {
-    estimated_cost: K,
-    cost: K,
-    index: usize,
+#[derive(Debug, Clone)]
+struct SearchSate {
+    cost: i64,
+    node: Node,
+    parent_id: Option<i64>,
 }
 
-impl<K: PartialEq> PartialEq for SmallestCostHolder<K> {
+impl PartialEq for SearchSate {
     fn eq(&self, other: &Self) -> bool {
-        self.estimated_cost.eq(&other.estimated_cost) && self.cost.eq(&other.cost)
+        self.node.id.eq(&other.node.id)
     }
 }
 
-impl<K: PartialEq> Eq for SmallestCostHolder<K> {}
+impl Eq for SearchSate {}
 
-impl<K: Ord> PartialOrd for SmallestCostHolder<K> {
+impl PartialOrd for SearchSate {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<K: Ord> Ord for SmallestCostHolder<K> {
+impl Ord for SearchSate {
     fn cmp(&self, other: &Self) -> Ordering {
-        match other.estimated_cost.cmp(&self.estimated_cost) {
+        match other.cost.cmp(&self.cost) {
             Ordering::Equal => self.cost.cmp(&other.cost),
             s => s,
         }
     }
-}
-
-#[allow(clippy::needless_collect)]
-fn reverse_path<N, V, F>(parents: FxIndexMap<N, V>, mut parent: F, start: usize) -> Vec<N>
-where
-    N: Eq + Hash + Clone,
-    F: FnMut(&V) -> usize,
-{
-    let mut i = start;
-    let path = std::iter::from_fn(|| {
-        parents.get_index(i).map(|(node, value)| {
-            i = parent(value);
-            node
-        })
-    })
-    .collect::<Vec<&N>>();
-    // Collecting the going through the vector is needed to revert the path because the
-    // unfold iterator is not double-ended due to its iterative nature.
-    path.into_iter().rev().cloned().collect()
 }
